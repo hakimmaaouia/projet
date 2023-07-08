2882812af0739096afd68f3e8b16cf0be6c7bdad
hakim maaouia <hakimmaaouia@gmail.com>
2023-07-08 21:25:15 +0100

    fix delete and import

--- delete.php
+++ delete.php
@@ -3,15 +3,13 @@ include "config.php";
 
 
 
-$id=$_GET['id'];
+$id = $_GET['id'];
 
-if(isset($_GET['id']))
+if (isset($_GET['id'])) {
 
-{
-
-    $stat=$db->prepare("DELETE FROM `demo` WHERE `demo`.`fid`='$id'");
-    $stat->bindparam(1,$id);
+    $stat = $db->prepare("DELETE FROM `demo` WHERE `fid` = ?");
+    $stat->bindparam(1, $id);
     $stat->execute();
-    header('REFRESH:.5;URL=ajouter.html');  
+    header('REFRESH:.5;URL=ajouter.html');
 
 }
--- import.php
+++ import.php
@@ -3,21 +3,24 @@ $filiere = $_POST['filiere'];
 $annee = $_POST['annee'];
 $semester = $_POST['semester'];
 $type = $_POST['type'];
+//file
 $doc_name = $_FILES['myfile']['name'];
 $doc_type = $_FILES['myfile']['type'];
 $doc_tmp = $_FILES['myfile']['tmp_name'];
 $doc_size = $_FILES['myfile']['size'];
+
+//setting 
 $errors = array();
-if ($doc_size > 400000000) {
+
+if ($doc_size > 400000) {
       $errors[] = '<div>fichier ne peut pas être plus de 400000 bit</div>';
 }
-
-
-$extontion = array('jpg', 'odt', 'pdf', 'txt', );
-$doc_extontion = strtolower(end(explode('.', $doc_name)));
-echo $doc_extontion;
-if (!(in_array($doc_extontion, $extontion))) {
-      $errors[] = '<div>fichier ne peut pas être ' . $doc_extontion . '</div>';
+$extontion = array('jpg', 'odt', 'pdf', 'txt');
+$doc_parts = explode('.', $doc_name);
+$doc_extension = strtolower(end($doc_parts));
+echo $doc_extension;
+if (!(in_array($doc_extension, $extontion))) {
+      $errors[] = '<div>fichier ne peut pas être ' . $doc_extension . '</div>';
 }
 
 if (empty($errors)) {
@@ -25,6 +28,8 @@ if (empty($errors)) {
       $conn = new mysqli("localhost", "root", "", "projet") or die(mysql_error($conn));
       $sql = "INSERT INTO `demo` VALUES ('1', NULL, '$doc_name', 'application/octet-stream', 'File Transfer', 'attachment', '0', 'must-revalidate', 'public','$filiere','$annee','$semester','$type');";
       $user = mysqli_query($conn, $sql);
+
+
 } else {
       foreach ($errors as $errors) {
             echo ($errors);
